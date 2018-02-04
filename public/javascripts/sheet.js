4df37c4cdc26f703bedc06da2bcfc04054c403e6
Traivok <jraf@cin.ufpe.br>
2018-02-04 18:47:28 -0300

    List creation JS

--- public/javascripts/sheet.js
+++ public/javascripts/sheet.js
@@ -1,22 +1,90 @@
 $(document).ready( function() {
 
+    /*
+     * Contents
+     * Variable Section
+     * Function Section
+     * Code Section
+     */    
+    
+    /*
+     * Variable Section
+     */
     var socket = io();
-    var rpgSystem = new RpgSystem( $('input[name=system-choice]:checked', '#system').val() );
+    var listing = true;
+    var sheetMan = { // sheet manager, select the correct sheet for a system and display it
+	display : function(foo) {
+	    
+	}
+    };
+    /*
+     * End of Variable Section
+     */
+    
 
-    var displaySheet = function () {};
+    /*
+     * Function Section
+     */
+    var content = function () { // display sheet list o sheet creation
+	if (listing) {
+	    //sheetListing();
+	} else {
+	    //sheetCreation();
+	}
+    };
+    
+    var swap = function () {
+	console.log("Swap to listing:" + listing);
+	if (listing) {
+	    $("#list").removeClass("active");
+	    $("#creation").addClass("active");
 
-    console.log("Tavola sheet manager started!");
-    console.log( rpgSystem.display() );
+	    $("#search-bar").remove();
+	    $('<li id="send-sheet"><button type="submit" class="btn btn-default navbar-btn">Save!</button></li>').insertAfter("#creation");
+	    
+	} else {
+	    $("#list").addClass("active");
+	    $("#creation").removeClass("active");
 
-    $("#system").change( function() {
+	    $("#send-sheet").remove();
+	    $('<form id="search-bar" class="navbar-form navbar-left">' + 
+	      '<div class="form-group">' +
+	      '<input type="text" class="form-control" placeholder="Search for a character">' +
+	      '</div>' +
+	      '<button type="submit" class="btn btn-default">Search</button>' +
+	      '</form>').	
+		insertAfter("#system-dropdown");
+	}
 	
+	listing = ! listing;
+	content();
+	
+    };
+    /*
+     * End of Function Section
+     */
+    
+
+    /*
+     * Code Section
+     */
+    console.log("Tavola sheet manager started!");
+    content();   
+    
+    
+    $("#system").change( function() {	
 	console.log("Form changed: " + $('input[name=system-choice]:checked', '#system').val());
-	displaySheet();
     });
 
+    $("#creation-a").bind("click", swap );
+    $("#list-a").bind("click", swap );
+    
+    /*
+     * End of Code Section
+     */
     
     
-
+    
     /* 	 $('<li><button id="send-sheet" type="submit" class="btn btn-default navbar-btn">Save!</button></li>').insertAfter("#creation");*/
 
 });
