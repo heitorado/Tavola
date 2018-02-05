6120b547eb56969f279527418235a9ff5c29e81b
Traivok <jraf@cin.ufpe.br>
2018-02-05 17:14:52 -0300

    Initiative basics complete

--- public/javascripts/initiative.js
+++ public/javascripts/initiative.js
@@ -27,14 +27,6 @@ $( document ).ready( function () {
     };
 
     var isGreater = function(a, b) { return b.ini - a.ini; };
-    
-    var displayCreatures = function() {
-	console.log("-----------------------");
-	for (var i = 0; i < creatures.length; i = i + 1) {
-	    console.log(creatures[i].name + " ==> " + creatures[i].ini);
-	}
-	console.log("-----------------------");
-    };
 
     var findWithAttr = function (array, attr, value) {
 	for(var i = 0; i < array.length; i += 1) {
@@ -52,22 +44,45 @@ $( document ).ready( function () {
 			      '<i class="fas fa-angle-up"></i>' +
 			      '<i class="fas fa-angle-down"></i>' +
 			      '</li>');};
+
+    var updateList = function() {
+	$("#ini-list").empty();
+	$(".cur").remove();
+
+	if ( creatures.length >= 0) {
+	    var name = creatures[0].name;
+	    var roll = creatures[0].roll;
+	    var mod = creatures[0].mod;
+
+	    $("#current-box").append('<p id="current" class="cur list-group-item">' + name + ' | Initiative: ' + (roll + mod) +' = ' + '<i class="ra  ra-dice-six"></i>(' + roll +') + ' + mod + '</p>' + '<hr class="cur" />');
+	}	
+	
+	creatures.forEach( function (c) {
+	    addToList(c.id, c.name, c.roll, c.mod);
+	});	
+    };
+
+    var exchange = function (array, i, j) {
+	array[i] = array.splice(j, 1, array[i])[0];
+    };
     /* End of Function Section */
 
     /* Code Section */
     $("#restart").click( function () {
 	if( confirm("You'll lost everything.") ) {
 	    console.log("Deleting all.");
+	    $("#ini-list").empty();
+	    creatures = [];
 	}
     });
 
     $("#new").click( function () {
 	if( confirm("You'll lost the current order.") ) {
 	    console.log("Sorting all.");
-	}
 
-	creatures.sort(isGreater);
-	displayCreatures();
+	    creatures.sort(isGreater);
+	    updateList();
+	}
     });
 
     $("#add").click( function () {
@@ -77,7 +92,6 @@ $( document ).ready( function () {
     $("#close-modal").click( function () {
 	$("#add-box").hide();
 	$("#char-values").text("Click to submit.");
-	displayCreatures();
     });
 
     $("#dice").click( function () {
@@ -89,20 +103,22 @@ $( document ).ready( function () {
 
 	old = creatures.slice();
 	creatures = tmp.slice();
-	
-	displayCreatures();
+
+	updateList();
     });
 
     $("#next").click( function () {
 	old = creatures.slice();
 	creatures.push(creatures.shift());
-	displayCreatures();
+
+	updateList();
     });
 
     $("#previous").click( function () {
 	old = creatures.slice();
 	creatures.unshift(creatures.pop());
-	displayCreatures();
+
+	updateList();
     });
     
     $("#submit-char").click( function () {
@@ -130,20 +146,38 @@ $( document ).ready( function () {
 	$("#char-values").text(name + " was submitted with initiative equals to " + (roll + mod) + ".");
 
 	// Adding char to html list
-	addToList(id, name, roll, mod);	
+	updateList();
     });
 
-    $(document).on("click", ".remove-char", function () {
-	console.log("Hei");
-	
+    $(document).on("click", ".remove-char", function () {	
 	var item = $(this).closest("li");
 	
 	var index = findWithAttr( creatures, 'id', item.attr("id") );
 	creatures.splice(index, 1);
-	
-	console.log("Removing:" + item.attr("id") );
-	
+		
 	item.remove();
+    });
+
+    $(document).on("click", ".fa-angle-up", function () {
+	var item = $(this).closest("li");
+
+	var index = findWithAttr( creatures, 'id', item.attr("id") );
+	exchange(creatures, index, (index - 1) % creatures.length);
+
+	updateList();	
+    });  
+
+    $(document).on("click", ".fa-angle-down", function () {
+	var item = $(this).closest("li");
+
+	var index = findWithAttr( creatures, 'id', item.attr("id") );
+	exchange(creatures, index, (index + 1) % creatures.length);
+
+	updateList();
+    });
+
+    $("#mechanics").click( function () {
+	alert("Not available yet.");
     });
     
     /* End of Code Section */    
